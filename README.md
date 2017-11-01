975506ef7db421ca3e0d8138c62a16992f86d312
William Emfinger <waemfinger@gmail.com>
2017-11-01 15:41:18 -0500

    trying to working connection to SD.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python-bluetooth
 Collection of python scripts to interact with BLE devices
+
+## Dependencies
+
+```bash
+pip install pygatt
+```
+
+## Modifications for Windows
+
+If you're running Windows, you'll need to modify the pip-installed version of pygatt according to [this issue](https://github.com/peplin/pygatt/issues/118)
--- smartDrive.py
+++ smartDrive.py
@@ -1,5 +1,6 @@
 import pygatt
 from binascii import hexlify
+import time
 
 adapter = pygatt.BGAPIBackend(serial_port='COM4')
 
@@ -13,8 +14,30 @@ def handle_data(handle, value):
 try:
     adapter.start()
     device = adapter.connect('00:07:80:a5:49:8c')
-
-    device.subscribe("e9add780b0424876aae1112855353cc1",
+    print('Connected')
+    #device.subscribe("a1e8f5b1-696b-4e4c-87c6-69dfe0b0093b"
+    #"e9add780-b042-4876-aae1-112855353cc1",
+    #"e8add780-b042-4876-aae1-112855353cc1",
+    device.subscribe("e7add780-b042-4876-aae1-112855353cc1",
                      callback=handle_data)
+    device.subscribe("e8add780-b042-4876-aae1-112855353cc1",
+                     callback=handle_data)
+    device.subscribe("e9add780-b042-4876-aae1-112855353cc1",
+                     callback=handle_data)
+    device.subscribe("eaadd780-b042-4876-aae1-112855353cc1",
+                     callback=handle_data)
+    device.subscribe("ebadd780-b042-4876-aae1-112855353cc1",
+                     callback=handle_data)
+
+    print('Subscribed, sleeping 10')
+    time.sleep(10)
+
+    value = device.char_read("e9add780-b042-4876-aae1-112855353cc1", 10)
+    print(value)
+    value = device.char_read("e8add780-b042-4876-aae1-112855353cc1", 10)
+    print(value)
+    value = device.char_read("e7add780-b042-4876-aae1-112855353cc1", 10)
+    print(value)
 finally:
+    print('Failed')
     adapter.stop()
