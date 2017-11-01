cde48dda3edc4709f2096573e71a5abcac971bd0
William Emfinger <waemfinger@gmail.com>
2017-11-01 15:44:58 -0500

    minor update.

--- smartDrive.py
+++ smartDrive.py
@@ -4,6 +4,16 @@ import time
 
 adapter = pygatt.BGAPIBackend(serial_port='COM4')
 
+smartDriveChars = [
+    "e7add780b0424876aae1112855353cc1",
+    "e8add780b0424876aae1112855353cc1",
+    "e9add780b0424876aae1112855353cc1",
+    "eaadd780b0424876aae1112855353cc1",
+    "ebadd780b0424876aae1112855353cc1"
+]
+
+dataChar = smartDriveChars[1]
+
 def handle_data(handle, value):
     """
     handle -- integer, characteristic read handle the data was received on
@@ -15,29 +25,17 @@ try:
     adapter.start()
     device = adapter.connect('00:07:80:a5:49:8c')
     print('Connected')
-    #device.subscribe("a1e8f5b1-696b-4e4c-87c6-69dfe0b0093b"
-    #"e9add780-b042-4876-aae1-112855353cc1",
-    #"e8add780-b042-4876-aae1-112855353cc1",
-    device.subscribe("e7add780-b042-4876-aae1-112855353cc1",
-                     callback=handle_data)
-    device.subscribe("e8add780-b042-4876-aae1-112855353cc1",
-                     callback=handle_data)
-    device.subscribe("e9add780-b042-4876-aae1-112855353cc1",
-                     callback=handle_data)
-    device.subscribe("eaadd780-b042-4876-aae1-112855353cc1",
-                     callback=handle_data)
-    device.subscribe("ebadd780-b042-4876-aae1-112855353cc1",
-                     callback=handle_data)
+
+    for char in smartDriveChars:
+        print ('Subscribing to: ' + char)
+        device.subscribe(char, callback=handle_data)
 
     print('Subscribed, sleeping 10')
     time.sleep(10)
 
-    value = device.char_read("e9add780-b042-4876-aae1-112855353cc1", 10)
+    value = device.char_read(dataChar, 10)
     print(value)
-    value = device.char_read("e8add780-b042-4876-aae1-112855353cc1", 10)
-    print(value)
-    value = device.char_read("e7add780-b042-4876-aae1-112855353cc1", 10)
-    print(value)
+
 finally:
     print('Failed')
     adapter.stop()
